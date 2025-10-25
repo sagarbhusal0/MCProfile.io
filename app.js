088f6f17c24a243f0285af3e6eb14c6edfb26552
Sagar Bhusal <127485363+sagarbhusal0@users.noreply.github.com>
2025-10-25 20:51:38 +0545

    Update app.js

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: false, // set this to true on production
+    secure: true, // set this to true on production
   },
 }))
 
